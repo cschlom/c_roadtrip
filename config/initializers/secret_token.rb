1ef3993f94f8c8ce9bf51761b7179c3dd092d99b
Christoph Schlom <CSchlom@gmail.com>
2012-03-18 03:35:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRoadtrip::Application.config.secret_token = '8d00b6e6965ad3ea9f2bf40b9a68cae7f33beca3fef950d5e1ff47a1d2b1936f0da219c2a25fd9466ab9c0c0f4b6dec5f537118816540e844ab32449e3990b7e'
